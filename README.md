c650edd703644328c7d3f2c0b7a4024ee04c5938
yelyzaveta samodid <samodid_y@qwasar.io>
2024-10-30 22:24:04 +0000

    Added README

--- README.md
+++ README.md
@@ -2,22 +2,35 @@
 ***
 
 ## Task
-TODO - What is the problem? And where is the challenge?
+The my_tar project involves implementing a simplified version of the tar command, called my_tar, which allows users to create, modify, list, and extract files within an archive. This command is specifically designed to handle single-level directories without recursion, meaning it does not support files inside subdirectories.
 
 ## Description
-TODO - How have you solved the problem?
+my_tar supports five main operations, indicated by the first option passed:
+
+	•	-c: Create a new archive with the specified files.
+	•	-r: Append new entries to an existing archive. Requires the -f option to specify the target archive.
+	•	-t: List the contents of an existing archive to standard output.
+	•	-u: Append only those entries to an existing archive that have been modified more recently than the corresponding archive entry. Requires the -f option.
+	•	-x: Extract contents of an existing archive. In cases where a file with the same name appears multiple times, each instance is extracted, with newer entries overwriting older ones.
 
 ## Installation
-TODO - How to install your project? npm install? make? make re?
+1. Clone the repository.
+2. Compile the program using the Makefile provided:
+   make
+   OR make re (if you need to recompile it)
+3. Run the program
 
 ## Usage
-TODO - How does it work?
+Run my_tar using the following format:
 ```
-./my_project argument1 argument2
+./my_tar -[mode option]f [archive name] [files...]
 ```
+Exit Status
+	•	0: Success
+	•	>0: Error occurred
 
 ### The Core Team
-
+Project completed by Nikita Gaidamachenko && Yelyzaveta Samodid
 
 <span><i>Made at <a href='https://qwasar.io'>Qwasar SV -- Software Engineering School</a></i></span>
 <span><img alt='Qwasar SV -- Software Engineering School's Logo' src='https://storage.googleapis.com/qwasar-public/qwasar-logo_50x50.png' width='20px' /></span>
--- my_tar.h
+++ my_tar.h
@@ -1,11 +1,13 @@
 #ifndef MY_TAR_H
 #define MY_TAR_H
+
 #define _POSIX_C_SOURCE 200809L
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <stdio.h>
 #include <dirent.h>
 #include <pwd.h>
@@ -13,33 +15,41 @@
 #include <utime.h>
 #include <string.h>
 #include <time.h>
+
 #define BLOCK_SIZE 512 
-struct tar_header {
+
+// Complex and strict tar header that is close the original
+typedef struct {
     char name[100];
     char mode[8];
     char uid[8];
     char gid[8];
     char size[12];
     char mtime[12];
-    char chksum[8];
+    char checksum[8];
     char typeflag;
-    char padding[355];
-};
-// Function Prototypes
-void pad_block(int fd, int size);
-int file_exists(const char *filename);
-long my_strtol(const char *nptr, char **endptr, int base);
-size_t my_strlen(const char *str);
+    char linkname[100];
+    char magic[6];
+    char version[2];
+    char uname[32];
+    char gname[32];
+    char devmajor[8];
+    char devminor[8];
+    char prefix[155];
+} tar_header;
+
+
 void write_error(const char *msg);
+int my_strlen(const char *str);
+void my_strncpy(char *dest, const char *src, int n);
 int my_strcmp(const char *str1, const char *str2);
-char *my_strncpy(char *dest, const char *src, size_t n);
-void write_cannot_open_tarball_error(const char *filename);
-void write_file_not_exist_error(const char *filename);
-void *my_memset(void *ptr, int value, size_t n);
-void my_itoa_octal_padded(char *buf, int value, int padding);
-void fill_tar_header(struct tar_header *header, const char *filename, struct stat *st);
-int add_file_to_tar(int tar_fd, const char *filename);
-void list_tar_contents(int tar_fd);
-int extract_files_from_tar(int tar_fd);
-int append_file_to_tar(int tar_fd, const char *filename);
+void my_memset(void *ptr, int value, size_t num);
+void my_itoa_octal(int num, char *str, int size);
+int my_strtol_octal(const char *str);
+int create_tar_header(const char *file_name, tar_header *header);
+int write_file_to_archive(int archive_fd, const char *file_name);
+int list_archive_contents(int archive_fd);
+int extract_archive(int archive_fd);
+int main(int argc, char *argv[]);
+
 #endif // MY_TAR_H
